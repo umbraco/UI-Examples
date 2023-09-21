0116e808bf73a2ff87332ad94ed92401e557f85c
Richard Ockerby <richardockerby@gmail.com>
2023-09-21 23:54:00 +0200

    Add a umb-box with some content in for the new dashboard

--- src/Our.Umbraco.UiExamples.v14/src/my-element.ts
+++ src/Our.Umbraco.UiExamples.v14/src/my-element.ts
@@ -1,7 +1,5 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
 
 /**
  * An example element.
@@ -25,56 +23,20 @@ export class MyElement extends LitElement {
   count = 0
 
   render() {
-    return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo small" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
+      return html`
+        <uui-box headline="UI Examples" headline-variant="h1">
+          <p>This package is developed and maintained by the Umbraco Package Team. Contributions are welcome: <a href="https://github.com/umbraco/UI-Examples" target="_blank">visit the GitHub repository</a>.</p>
+            <p>It contains an overview of several backoffice elements and shows you how to use them. Each UI example is linked to from a tab in the section header.</p>
+            <p>The package is also great reference material, if you see something within this section - that isn't covered on one of the tabs - you can go to your App_Plugins folder and check the source.</p>
+            <p>Everything in this package is front-end only (except for a bit of code that automatically adds this section to all user groups).</p>
+        </uui-box>
     `
-  }
-
-  private _onClick() {
-    this.count++
   }
 
   static styles = css`
     :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
-    }
-
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
+      padding: var(--uui-size-layout-1);
+      display:block;
     }
 
     ::slotted(h1) {
@@ -89,25 +51,6 @@ export class MyElement extends LitElement {
     }
     a:hover {
       color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
     }
 
     @media (prefers-color-scheme: light) {
