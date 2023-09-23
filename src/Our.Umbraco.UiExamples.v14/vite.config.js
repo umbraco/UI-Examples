8a4719eb477f9f8075b76d19c1560a56a1b7848e
Richard Ockerby <richardockerby@gmail.com>
2023-09-23 15:16:27 +0200

    Update the way the rollup config looks for files so it automatically picks up new files within the src folder Add a section view example

--- src/Our.Umbraco.UiExamples.v14/vite.config.js
+++ src/Our.Umbraco.UiExamples.v14/vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig } from "vite";
+import {glob} from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
     build: {
         lib: {
-            entry: ["src/dashboards/welcome-dashboard.ts", "src/dashboards/custom-dialogs-dashboard.ts"], // your web component source file
+            entry: ["src/dashboards/welcome-dashboard.ts"], // your web component source file
             formats: ["es"],
         },
         outDir: "../../samples/Umbraco14.Website/App_Plugins/Example.UI/scripts", // your web component will be saved in this location
         sourcemap: true,
         rollupOptions: {
+            input: Object.fromEntries(
+                glob.sync('src/**/*.ts').map(file => [
+                    // This remove `src/` as well as the file extension from each
+                    // file, so e.g. src/nested/foo.js becomes nested/foo
+                    path.relative(
+                        'src',
+                        file.slice(0, file.length - path.extname(file).length)
+                    ),
+                    // This expands the relative paths to absolute paths, so e.g.
+                    // src/nested/foo becomes /project/src/nested/foo.js
+                    fileURLToPath(new URL(file, import.meta.url))
+                ])
+            ),
             external: [/^@umbraco/],
         },
     },
