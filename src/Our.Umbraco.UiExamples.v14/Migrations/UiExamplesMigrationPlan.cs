15335a1f500da72c05bdfa7a5a8626d2fd0344c3
Ronald Barendse <ronald@barend.se>
2024-09-06 10:24:39 +0200

    Add section to admin group using package migration

---
+++ src/Our.Umbraco.UiExamples.v14/Migrations/UiExamplesMigrationPlan.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Cms.Core.Packaging;
+
+namespace Our.Umbraco.UiExamples.Migrations;
+
+internal sealed class UiExamplesMigrationPlan : PackageMigrationPlan
+{
+    public UiExamplesMigrationPlan()
+        : base("Our.Umbraco.UiExamples", "UI Examples", "UiExamples")
+    { }
+
+    public override bool IgnoreCurrentState => false;
+
+    protected override void DefinePlan()
+        => To<AddSectionToAdminsMigration>("AddedSectionForAdmins-Ran");
+}
